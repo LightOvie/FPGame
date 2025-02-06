b1c69cf496b1c1224939b670398e3a218170d1ff
Svyatoslav <slavalapshin17@gmail.com>
2025-02-06 13:45:29 +0100

    new aaasets for the scene

--- Assets/Script/UI Scripts/SceneController.cs
+++ Assets/Script/UI Scripts/SceneController.cs
@@ -4,14 +4,37 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 public class SceneController : MonoBehaviour
 {
+	[SerializeField] private AudioClip clickClips;
+	[SerializeField] private GameObject howToPlayPanel;
+
+	private void Awake()
+	{
+		
+		howToPlayPanel.SetActive(false);
+	}
+
+	public void ActivePanel()
+	{
+		SoundFXManager.instance.PlaySoundFXClip(clickClips, transform, 1f);
+		howToPlayPanel.SetActive(true);
+	}
+
+	public void ClosePanel()
+	{
+		SoundFXManager.instance.PlaySoundFXClip(clickClips, transform, 1f);
+		howToPlayPanel.SetActive(false);
+	}
+
 	public void SceneChange(string name)
 	{
 		SceneManager.LoadScene(name);
 		Time.timeScale = 1.0f;
+		SoundFXManager.instance.PlaySoundFXClip(clickClips, transform, 1f);
 	}
 
 	public void ExitFromGamee()
 	{
+		SoundFXManager.instance.PlaySoundFXClip(clickClips, transform, 1f);
 		Application.Quit();
 	}
 
